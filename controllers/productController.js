8d05048ea7ab313706880e838dcfe8e0d24acb7a
Thriven Komarr <thrivenkomarr@gmail.com>
2026-03-06 01:00:44 -0800

    html_adding- Ecommerce API using Express

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,11 @@
 const productService = require("../services/productService");
+const path = require("path");
 
 // GET /products
 exports.getAllProducts = (req, res) => {
     const result = productService.getAllProducts();
-    res.status(200).send(result);
+    console.log(result);
+    res.status(200).sendFile(path.join(__dirname, "../view/product.html"));
 };
 
 // GET /products/:id
